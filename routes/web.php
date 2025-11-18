7884bfd03b10af96ec44ad92566ded99aaba0278
starrr <rbmalasarte2020@gmail.com>
2025-11-18 20:26:55 +0800

    asdasd
    asdasdasd

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('portfolio');
 });
-
-Route::get('/', function () {
-    return view('portfolio');
-})->name('portfolio');
